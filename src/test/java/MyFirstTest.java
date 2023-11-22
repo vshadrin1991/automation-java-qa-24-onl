018005f73cb04bd3775e84ab95a1b0ae100a3c9e
vshadrin1991 <vital.shadrin2@gmail.com>
2023-11-22 10:11:39 +0300

    lecture 3

---
+++ src/test/java/MyFirstTest.java
@@ -0,0 +1,17 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class MyFirstTest {
+    String str = "Hello";
+
+
+    @Test
+    public void test1() {
+        Assert.assertFalse(str.isEmpty(), "String is empty!");
+    }
+
+    @Test
+    public void test2() {
+        Assert.assertNotEquals(str, null);
+    }
+}
